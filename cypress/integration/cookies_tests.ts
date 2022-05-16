1024843cd526c16252bb97ff6586ff7da6e48064
Stalkakuma <stalkakuma@gmail.com>
2022-05-16 22:22:29 +0300

    expect fail

--- cypress/integration/cookies_tests.ts
+++ cypress/integration/cookies_tests.ts
@@ -8,10 +8,13 @@ describe('Cookie test', () => {
     renderHook(() => useCookieConsent());
     expect(cy.getCookie(COOKIE_CONSENT_KEY)).to.exist;
   });
-  it('should fail', () => {
-    renderHook(() => useCookieConsent());
-    expect(cy.getCookie(COOKIE_CONSENT_KEY)).to.not.exist;
-  });
+
+  expect(
+    it('should fail', () => {
+      renderHook(() => useCookieConsent());
+      expect(cy.getCookie(COOKIE_CONSENT_KEY)).to.not.exist;
+    })
+  ).to.throw(Error);
 
   it('should call cookie hook, then update cookie value', () => {
     const { result } = renderHook(() => useCookieConsent());
